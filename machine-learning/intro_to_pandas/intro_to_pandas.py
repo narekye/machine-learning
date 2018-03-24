b84cf1d53ec31f81126603e4a55b6f7c27a7bae9
Narek Yegoryan <yegoryan.narek@gmail.com>
2018-03-24 13:43:47 +0400

    importing pandas and numpy

--- machine-learning/intro_to_pandas/intro_to_pandas.py
+++ machine-learning/intro_to_pandas/intro_to_pandas.py
@@ -1,1 +1,19 @@
 
+import pandas as pd
+import numpy as np
+
+print(pd.__version__)
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469 ,1015785, 485199])
+
+pd.DataFrame({'City name' : city_names, 'Population' : population })
+
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe.describe()
+
+california_housing_dataframe.head()
+
+# california_housing_dataframe.hist('housing_median_age')
+
+print(np.log(population))
